38a73370c563c6f3437a21973f4d02baad197acb
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-10-26 22:47:14 +0300

    Created DAO: class Users

---
+++ myBrikProject/src/main/java/by/mybrik/dao/Users.java
@@ -0,0 +1,48 @@
+package by.mybrik.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.sql.Timestamp;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users {
+
+    private Long id;
+
+    private String name;
+
+    private String surName;
+
+    private String login;
+
+    private String password;
+
+    private String email;
+
+    private String gender;
+
+    private Timestamp created = new Timestamp(System.currentTimeMillis());
+
+    private Timestamp changed = new Timestamp(System.currentTimeMillis());
+
+    private int phone;
+
+    private String address;
+
+    private boolean isDeleted;
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+    }
+}
