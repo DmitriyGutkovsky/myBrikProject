f2bece8ab4cd7af3937cc1022af42120826a61ce
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-10-27 14:50:06 +0300

    Domain: created class ProductType

---
+++ myBrikProject/src/main/java/by/mybrik/domain/ProductType.java
@@ -0,0 +1,35 @@
+package by.mybrik.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.sql.Timestamp;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductType {
+
+    private long id;
+
+    private String productType;
+
+    private boolean isDeleted;
+
+    private Timestamp created = new Timestamp(System.currentTimeMillis());
+
+    private Timestamp changed = new Timestamp(System.currentTimeMillis());
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+    }
+
+}
