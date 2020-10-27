2f8808eb6f0ffdbefb3e953735ec315d749ac34a
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-10-27 14:53:33 +0300

    Domain: created class PriceForIndividualOrder

---
+++ myBrikProject/src/main/java/by/mybrik/domain/PriceForIndividualOrder.java
@@ -0,0 +1,37 @@
+package by.mybrik.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.sql.Timestamp;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class PriceForIndividualOrder {
+
+    private long id;
+
+    private String product_type;
+
+    private double price;
+
+    private Timestamp created = new Timestamp(System.currentTimeMillis());
+
+    private Timestamp changed = new Timestamp(System.currentTimeMillis());
+
+    private boolean isDeleted;
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+    }
+
+}
