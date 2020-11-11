13e07416e64acca39f0a49e2cd6fbec80fcaac37
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-11 09:32:15 +0300

    Execution queries to database: created TesterJDBCTemplate class for testing queries to DB

---
+++ myBrikProject/src/main/java/by/mybrik/repository/TesterJDBCTemplate.java
@@ -0,0 +1,32 @@
+package by.mybrik.repository;
+
+import by.mybrik.domain.Goods;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class TesterJDBCTemplate {
+
+  public static void main(String[] args) {
+
+      AnnotationConfigApplicationContext annotationConfigApplicationContext =
+              new AnnotationConfigApplicationContext("by.mybrik");
+
+
+      GoodsRepository productBean = annotationConfigApplicationContext.getBean(GoodsRepository.class);
+
+      Goods productForSaving = Goods.builder()
+              .orderCode("someOrderCode6")
+              .name("cap")
+              .photo("linkToPhoto")
+              .gender("Male")
+              .size("52")
+              .color("red")
+              .description("summer cap")
+              .isDeleted(false)
+              .price(15)
+              .quantity(15)
+              .category("caps")
+              .build();
+
+        System.out.println(productBean.save(productForSaving));
+  }
+}
