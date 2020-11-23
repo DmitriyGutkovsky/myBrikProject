5f22d36175a9a37b182268cfea26da1c4983873f
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-23 18:37:41 +0300

    GoodsRestController - started

---
+++ myBrikProject/src/main/java/by/mybrik/controller/GoodsRestController.java
@@ -0,0 +1,13 @@
+package by.mybrik.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("rest/goods")
+@RequiredArgsConstructor
+public class GoodsRestController {
+
+}
