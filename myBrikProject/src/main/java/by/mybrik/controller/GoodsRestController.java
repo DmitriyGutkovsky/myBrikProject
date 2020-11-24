8fe6f5fa854953bb043c8d3fdf2126e88048c078
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-24 13:31:34 +0300

    GoodsRestController: implemented method findAllGoods(): GET + http://localhost:8080/rest/goods

--- myBrikProject/src/main/java/by/mybrik/controller/GoodsRestController.java
+++ myBrikProject/src/main/java/by/mybrik/controller/GoodsRestController.java
@@ -1,13 +1,30 @@
 package by.mybrik.controller;
 
+import by.mybrik.domain.Goods;
+import by.mybrik.service.GoodsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("rest/goods")
 @RequiredArgsConstructor
 public class GoodsRestController {
+
+    public final GoodsService goodsService;
+
+    //http://localhost:8080/rest/goods
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Goods> findAllGoods(){
+        return goodsService.findAll();
+    }
+
+
 
 }
