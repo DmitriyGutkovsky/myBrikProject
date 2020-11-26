7819a2665a4b792e575985d6e336a693b4a19787
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-26 13:00:14 +0300

    UsersRestController: implemented method findUserById(): GET + http://localhost:8080/rest/users/18

--- myBrikProject/src/main/java/by/mybrik/controllers/UsersRestController.java
+++ myBrikProject/src/main/java/by/mybrik/controllers/UsersRestController.java
@@ -4,10 +4,7 @@ import by.mybrik.domain.Users;
 import by.mybrik.service.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,13 @@ public class UsersRestController {
     @ResponseStatus(HttpStatus.OK)
     public List<Users> getAllUsers(){
         return usersService.findAll();
+    }
+
+    // http://localhost:8080/rest/users/18
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Users findUserById(@PathVariable Long id){
+        return usersService.findById(id);
     }
 
 }
