d56772ce9a483d545de5ba171dbe6781665fbdf2
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-27 19:43:28 +0300

    Implemented service layout for Textile table

---
+++ myBrikProject/src/main/java/by/mybrik/service/TextileService.java
@@ -0,0 +1,22 @@
+package by.mybrik.service;
+
+import by.mybrik.domain.Textile;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TextileService {
+
+    Textile save(Textile textile);
+
+    List<Textile> findAll();
+
+    Textile findById(Long id);
+
+    Optional<Textile> findOne(Long id);
+
+    Textile update(Textile textile);
+
+    Long delete(Textile textile);
+
+}
