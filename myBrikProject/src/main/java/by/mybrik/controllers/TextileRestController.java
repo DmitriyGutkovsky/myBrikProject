e487dc1740e81be2f1f30d0949af21e04cb85117
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-27 20:07:51 +0300

    Implementation TextileRestController : implemented method deleteTextileById()

--- myBrikProject/src/main/java/by/mybrik/controllers/TextileRestController.java
+++ myBrikProject/src/main/java/by/mybrik/controllers/TextileRestController.java
@@ -15,16 +15,26 @@ public class TextileRestController {
 
     public final TextileService textileService;
 
+    // http://localhost:8080/rest/textile
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Textile> getListOfAllTextile(){
         return textileService.findAll();
     }
 
+    // http://localhost:8080/rest/textile/5
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Textile findTextileById(@PathVariable("id") Long id){
         return textileService.findById(id);
+    }
+
+    // http://localhost:8080/rest/textile/5
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Textile> deleteTextileById(@PathVariable("id") Long id){
+        textileService.delete(textileService.findById(id));
+        return textileService.findAll();
     }
 
 }
