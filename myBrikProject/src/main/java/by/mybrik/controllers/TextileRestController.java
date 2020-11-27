6adb92a2f756d44fd7462ed26e44beb22b0f8f39
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-27 19:56:54 +0300

    Implementation TextileRestController : implemented method getListOfAllTextile()

--- myBrikProject/src/main/java/by/mybrik/controllers/TextileRestController.java
+++ myBrikProject/src/main/java/by/mybrik/controllers/TextileRestController.java
@@ -1,9 +1,15 @@
 package by.mybrik.controllers;
 
+import by.mybrik.domain.Textile;
 import by.mybrik.service.TextileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/rest/textile")
@@ -11,5 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TextileRestController {
 
     public final TextileService textileService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Textile> getListOfAllTextile(){
+        return textileService.findAll();
+    }
 
 }
