e92e7b3c4cc12fb70473e9a4b478b66ee2a8a5ea
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-28 18:15:07 +0300

    Implementation ProductTypeRestController - implemented ProductTypeUpdateRequest class

---
+++ myBrikProject/src/main/java/by/mybrik/controllers/requests/productTypeRequests/ProductTypeUpdateRequest.java
@@ -0,0 +1,10 @@
+package by.mybrik.controllers.requests.productTypeRequests;
+
+import lombok.Data;
+
+@Data
+public class ProductTypeUpdateRequest extends ProductTypeCreateRequest {
+
+    private Long id;
+
+}
