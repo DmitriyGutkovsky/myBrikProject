bc0c6336596eca0b4e1d104b52e986f40e467e9b
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-28 16:35:26 +0300

    Implementation ProductTypeRestController - implemented method getListOfAllProductTypes() - added HttpStatus for response

--- myBrikProject/src/main/java/by/mybrik/controllers/ProductTypeRestController.java
+++ myBrikProject/src/main/java/by/mybrik/controllers/ProductTypeRestController.java
@@ -3,8 +3,10 @@ package by.mybrik.controllers;
 import by.mybrik.domain.ProductType;
 import by.mybrik.service.ProductTypeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,8 +19,11 @@ public class ProductTypeRestController {
     public final ProductTypeService typeService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<ProductType> getListOfAllProductTypes(){
         return typeService.findAll();
     }
+
+
 
 }
