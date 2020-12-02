0900368788ab84712ef811277e3ba955a62cb92a
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-02 09:25:05 +0300

    Implementation PriceForIndividualOrderRestController - implemented method getIndividualOrderPriceById()

--- myBrikProject/src/main/java/by/mybrik/controllers/PriceForIndividualOrderRestController.java
+++ myBrikProject/src/main/java/by/mybrik/controllers/PriceForIndividualOrderRestController.java
@@ -5,6 +5,7 @@ import by.mybrik.service.PriceForIndividualOrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,15 @@ public class PriceForIndividualOrderRestController {
     public List<PriceForIndividualOrder> getListOfAllPrices(){
         return priceForIndividualOrderService.findAll();
     }
+
+    // http://localhost:8080/rest/individualorderprice/3
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PriceForIndividualOrder getIndividualOrderPriceById(@PathVariable("id") Long id){
+        return priceForIndividualOrderService.findById(id);
+    }
+
+
 
 
 }
