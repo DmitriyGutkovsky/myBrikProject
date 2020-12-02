ceee83ccf12d94820abe257d0ce07479b9cfe4b6
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-02 09:08:02 +0300

    Implemented service layout for m_price_for_individual_order table

---
+++ myBrikProject/src/main/java/by/mybrik/service/PriceForIndividualOrderService.java
@@ -0,0 +1,24 @@
+package by.mybrik.service;
+
+
+import by.mybrik.domain.PriceForIndividualOrder;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PriceForIndividualOrderService {
+
+    PriceForIndividualOrder save(PriceForIndividualOrder price);
+
+    List<PriceForIndividualOrder> findAll();
+
+    PriceForIndividualOrder findById(Long id);
+
+    Optional<PriceForIndividualOrder> findOne(Long id);
+
+    PriceForIndividualOrder update(PriceForIndividualOrder price);
+
+    Long delete(PriceForIndividualOrder price);
+
+
+}
---
+++ myBrikProject/src/main/java/by/mybrik/service/impl/PriceForIndividualOrderServiceImpl.java
@@ -0,0 +1,48 @@
+package by.mybrik.service.impl;
+
+import by.mybrik.domain.PriceForIndividualOrder;
+import by.mybrik.repository.PriceForIndividualOrderRepository;
+import by.mybrik.service.PriceForIndividualOrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class PriceForIndividualOrderServiceImpl implements PriceForIndividualOrderService {
+
+    private final PriceForIndividualOrderRepository priceForIndividualOrderRepository;
+
+
+    @Override
+    public PriceForIndividualOrder save(PriceForIndividualOrder price) {
+        return priceForIndividualOrderRepository.save(price);
+    }
+
+    @Override
+    public List<PriceForIndividualOrder> findAll() {
+        return priceForIndividualOrderRepository.findAll();
+    }
+
+    @Override
+    public PriceForIndividualOrder findById(Long id) {
+        return priceForIndividualOrderRepository.findById(id);
+    }
+
+    @Override
+    public Optional<PriceForIndividualOrder> findOne(Long id) {
+        return priceForIndividualOrderRepository.findOne(id);
+    }
+
+    @Override
+    public PriceForIndividualOrder update(PriceForIndividualOrder price) {
+        return priceForIndividualOrderRepository.update(price);
+    }
+
+    @Override
+    public Long delete(PriceForIndividualOrder price) {
+        return priceForIndividualOrderRepository.delete(price);
+    }
+}
