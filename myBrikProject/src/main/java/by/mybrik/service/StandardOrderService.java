6e9c711dac79a209c49d79f6719122c01b42d417
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-02 23:53:19 +0300

    Implemented service layout for m_standard_order table

---
+++ myBrikProject/src/main/java/by/mybrik/service/StandardOrderService.java
@@ -0,0 +1,22 @@
+package by.mybrik.service;
+
+import by.mybrik.domain.StandardOrder;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StandardOrderService {
+
+    StandardOrder save(StandardOrder order);
+
+    List<StandardOrder> findAll();
+
+    StandardOrder findById(Long id);
+
+    Optional<StandardOrder> findOne(Long id);
+
+    StandardOrder update(StandardOrder order);
+
+    Long delete(StandardOrder order);
+
+}
