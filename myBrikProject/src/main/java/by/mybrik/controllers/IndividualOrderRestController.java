ffb14643ebec747bc733a1a869c6020dda3276ad
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-03 13:02:41 +0300

    Implementation IndividualOrderRestController - implemented method deleteIndividualOrder()

--- myBrikProject/src/main/java/by/mybrik/controllers/IndividualOrderRestController.java
+++ myBrikProject/src/main/java/by/mybrik/controllers/IndividualOrderRestController.java
@@ -1,11 +1,15 @@
 package by.mybrik.controllers;
 
 import by.mybrik.domain.IndividualOrder;
-import by.mybrik.domain.StandardOrder;
 import by.mybrik.service.IndividualOrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -30,5 +34,13 @@ public class IndividualOrderRestController {
         return individualOrderService.findById(id);
     }
 
+    // http://localhost:8080/rest/individualorder/1
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<IndividualOrder>  deleteIndividualOrder(@PathVariable Long id){
+        IndividualOrder oderFordelete = individualOrderService.findById(id);
+        individualOrderService.delete(oderFordelete);
+        return individualOrderService.findAll();
+    }
 
 }
