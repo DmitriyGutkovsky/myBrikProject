c276b0245178347c6c094836c39d04eefac1be3f
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-03 12:42:03 +0300

    Implemented Service layout for m_individual_order table

---
+++ myBrikProject/src/main/java/by/mybrik/service/impl/IndividualOrderServiceImpl.java
@@ -0,0 +1,47 @@
+package by.mybrik.service.impl;
+
+import by.mybrik.domain.IndividualOrder;
+import by.mybrik.repository.IndividualOrderRepository;
+import by.mybrik.service.IndividualOrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class IndividualOrderServiceImpl implements IndividualOrderService {
+
+    private final IndividualOrderRepository orderRepository;
+
+    @Override
+    public IndividualOrder save(IndividualOrder order) {
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public List<IndividualOrder> findAll() {
+        return orderRepository.findAll();
+    }
+
+    @Override
+    public IndividualOrder findById(Long id) {
+        return orderRepository.findById(id);
+    }
+
+    @Override
+    public Optional<IndividualOrder> findOne(Long id) {
+        return orderRepository.findOne(id);
+    }
+
+    @Override
+    public IndividualOrder update(IndividualOrder order) {
+        return orderRepository.update(order);
+    }
+
+    @Override
+    public Long delete(IndividualOrder order) {
+        return orderRepository.delete(order);
+    }
+}
