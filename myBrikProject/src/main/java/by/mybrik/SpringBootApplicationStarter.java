c6d2343a56e73d4b9bc48a3e957921b37ce54a3d
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-07 22:08:23 +0300

    Hibernate: implemented config class for configuration a bean of sessionFactory

--- myBrikProject/src/main/java/by/mybrik/SpringBootApplicationStarter.java
+++ myBrikProject/src/main/java/by/mybrik/SpringBootApplicationStarter.java
@@ -1,6 +1,7 @@
 package by.mybrik;
 
 import by.mybrik.config.ApplicationBeans;
+import by.mybrik.config.PersistenceContextBeansConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -11,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //@EnableWebMvc
 @EnableSwagger2
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-@Import({ApplicationBeans.class})
+@Import({ApplicationBeans.class, PersistenceContextBeansConfiguration.class})
 public class SpringBootApplicationStarter {
 
   public static void main(String[] args) {
