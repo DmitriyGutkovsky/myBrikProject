1a0ad56c72d32b83bcad7decf7aafe48c187c6b1
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-08 23:29:00 +0300

    Hibernate: implemented new TextileController - methods updateTextile() and deleteTextileById()

--- myBrikProject/src/main/java/by/mybrik/controllers/newRevision/TextileController.java
+++ myBrikProject/src/main/java/by/mybrik/controllers/newRevision/TextileController.java
@@ -1,18 +1,22 @@
 package by.mybrik.controllers.newRevision;
 
 import by.mybrik.controllers.requests.textileRequests.TextileCreate;
+import by.mybrik.controllers.requests.textileRequests.TextileUpdate;
 import by.mybrik.domain.entities.Textile;
 import by.mybrik.service.newImplementation.TextileSer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @RestController
@@ -61,5 +65,41 @@ public class TextileController {
     textile.setDeleted(request.isDeleted());
 
     return textileService.save(textile);
+  }
+
+  /*
+ http://localhost:8080/new/rest/textile/6
+  {
+     "code": "someCodeUpdate",
+     "name": "someName6",
+     "color": "someColor",
+     "description": "someDescription",
+     "photo": "somePhoto",
+     "deleted": false
+ }
+  */
+  @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public Textile updateTextile(@PathVariable("id") Long id, @RequestBody TextileUpdate request){
+
+    Textile updatedTextile = textileService.findById(id);
+
+    updatedTextile.setCode(request.getCode());
+    updatedTextile.setName(request.getName());
+    updatedTextile.setColor(request.getColor());
+    updatedTextile.setDescription(request.getDescription());
+    updatedTextile.setPhoto(request.getPhoto());
+    updatedTextile.setDeleted(request.isDeleted());
+    updatedTextile.setChanged(new Timestamp(System.currentTimeMillis()));
+
+    return textileService.update(updatedTextile);
+  }
+
+  // http://localhost:8080/new/rest/textile/6
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public List<Textile> deleteTextileById(@PathVariable("id") Long id){
+    textileService.delete(textileService.findById(id));
+    return textileService.findAll();
   }
 }
--- myBrikProject/src/main/java/by/mybrik/repository/newImplementation/implementation/TextileImpl.java
+++ myBrikProject/src/main/java/by/mybrik/repository/newImplementation/implementation/TextileImpl.java
@@ -5,6 +5,7 @@ import by.mybrik.repository.newImplementation.TextileRep;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
@@ -16,43 +17,55 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class TextileImpl implements TextileRep {
 
-    private final SessionFactory sessionFactory;
+  private final SessionFactory sessionFactory;
 
-    @Override
-    public Textile save(Textile textile) {
-        try(Session session = sessionFactory.openSession()){
-            session.saveOrUpdate(textile);
-            return textile;
-        }
+  @Override
+  public Textile save(Textile textile) {
+    try (Session session = sessionFactory.openSession()) {
+      session.saveOrUpdate(textile);
+      return textile;
     }
+  }
 
-    @Override
-    public List<Textile> findAll() {
-        try(Session session = sessionFactory.openSession()){
-            String hqlQuery = "select u from Textile u";
-            return session.createQuery(hqlQuery, Textile.class).list();
-        }
+  @Override
+  public List<Textile> findAll() {
+    try (Session session = sessionFactory.openSession()) {
+      String hqlQuery = "select u from Textile u";
+      return session.createQuery(hqlQuery, Textile.class).list();
     }
+  }
 
-    @Override
-    public Textile findById(Long key) {
-        try(Session session = sessionFactory.openSession()){
-            return session.find(Textile.class, key);
-        }
+  @Override
+  public Textile findById(Long key) {
+    try (Session session = sessionFactory.openSession()) {
+      return session.find(Textile.class, key);
     }
+  }
 
-    @Override
-    public Optional<Textile> findOne(Long key) {
-        return Optional.empty();
-    }
+  @Override
+  public Optional<Textile> findOne(Long key) {
+    return Optional.empty();
+  }
 
-    @Override
-    public Textile update(Textile object) {
-        return null;
+  @Override
+  public Textile update(Textile textile) {
+    try (Session session = sessionFactory.openSession()) {
+      Transaction transaction = session.getTransaction();
+      transaction.begin();
+      session.saveOrUpdate(textile);
+      transaction.commit();
+      return textile;
     }
+  }
 
-    @Override
-    public Long delete(Textile object) {
-        return null;
+  @Override
+  public Long delete(Textile textile) {
+    try (Session session = sessionFactory.openSession()) {
+      Transaction transaction = session.getTransaction();
+      transaction.begin();
+      session.delete(textile);
+      transaction.commit();
+      return textile.getId();
     }
+  }
 }
