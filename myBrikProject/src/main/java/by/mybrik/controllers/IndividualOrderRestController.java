1637a68bd2523e5a02e7b001cee6f7c3a7b37f74
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-09 13:32:08 +0300

    new branch: on JdbcTemplate

--- myBrikProject/src/main/java/by/mybrik/controllers/IndividualOrderRestController.java
+++ myBrikProject/src/main/java/by/mybrik/controllers/IndividualOrderRestController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.sql.Timestamp;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("rest/individualorder")
 @RequiredArgsConstructor
