757e28d5aa2341a25275bb74c400e06175ab0fac
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-11 20:16:53 +0300

    Replaced implementation from Hibernate to Spring Data: UsersController implemented methods: getListOfAllTextile(), findTextileById(), deleteTextileById()

---
+++ myBrikProject/src/main/java/by/mybrik/repository/impl/TextileRepository.java
@@ -0,0 +1,7 @@
+package by.mybrik.repository.impl;
+
+import by.mybrik.domain.Textile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TextileRepository extends JpaRepository<Textile, Long> {
+}
