1485fdafb178d84f7a2d8d7f91de91ce093a1112
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-11 22:17:42 +0300

    Replaced implementation from Hibernate to Spring Data: PriceForIndividualOrderController implemented methods: addPriceForIndividualOrder(), updatePriceForIndividualOrder()

--- myBrikProject/src/main/java/by/mybrik/controllers/PriceForIndividualOrderController.java
+++ myBrikProject/src/main/java/by/mybrik/controllers/PriceForIndividualOrderController.java
@@ -1,5 +1,7 @@
 package by.mybrik.controllers;
 
+import by.mybrik.controllers.requests.priceForIndividualRequests.PriceForIndividualCreate;
+import by.mybrik.controllers.requests.priceForIndividualRequests.PriceForIndividualUpdate;
 import by.mybrik.domain.PriceForIndividualOrder;
 import by.mybrik.repository.impl.PriceForIndividualOrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,10 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
@@ -48,46 +54,52 @@ public class PriceForIndividualOrderController {
     return priceForIndividualOrderRepository.findAll();
   }
 
-  //    /*
-  //    http://localhost:8080/new/rest/individualorderprice
-  //    {
-  //        "productType": "test",
-  //        "price": 12.0,
-  //        "deleted": false
-  //    }
-  //     */
-  //    @PostMapping
-  //    @ResponseStatus(HttpStatus.CREATED)
-  //    public PriceForIndividualOrder addPriceForIndividualOrder(@RequestBody
-  // PriceForIndividualCreate request ){
-  //
-  //        PriceForIndividualOrder price = new PriceForIndividualOrder();
-  //        price.setProductType(request.getProductType());
-  //        price.setPrice(request.getPrice());
-  //        price.setDeleted(request.isDeleted());
-  //
-  //        return priceForIndividualOrderService.save(price);
-  //    }
-  //
-  //    /*
-  //    http://localhost:8080/new/rest/individualorderprice/4
-  //    {
-  //        "productType": "test",
-  //        "price": 12.0,
-  //        "deleted": false
-  //    }
-  //    */
-  //    @PutMapping("/{id}")
-  //    @ResponseStatus(HttpStatus.OK)
-  //    public PriceForIndividualOrder updatePriceForIndividualOrder(@PathVariable ("id") Long id,
-  // @RequestBody PriceForIndividualUpdate request ){
-  //
-  //        PriceForIndividualOrder price = priceForIndividualOrderService.findById(id);
-  //        price.setProductType(request.getProductType());
-  //        price.setPrice(request.getPrice());
-  //        price.setDeleted(request.isDeleted());
-  //        price.setChanged(new Timestamp(System.currentTimeMillis()));
-  //
-  //        return priceForIndividualOrderService.update(price);
-  //    }
+  /*
+  http://localhost:8080/new/rest/individualorderprice
+  {
+      "productType": "test",
+      "price": 12.0,
+      "deleted": false
+  }
+   */
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public PriceForIndividualOrder addPriceForIndividualOrder(
+      @RequestBody PriceForIndividualCreate request) {
+
+    PriceForIndividualOrder price = new PriceForIndividualOrder();
+    price.setProductType(request.getProductType());
+    price.setPrice(request.getPrice());
+    price.setDeleted(request.isDeleted());
+
+    return priceForIndividualOrderRepository.save(price);
+  }
+
+  /*
+  http://localhost:8080/new/rest/individualorderprice/4
+  {
+      "productType": "test",
+      "price": 12.0,
+      "deleted": false
+  }
+  */
+  @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public PriceForIndividualOrder updatePriceForIndividualOrder(
+      @PathVariable("id") Long id, @RequestBody PriceForIndividualUpdate request) {
+
+    if (!priceForIndividualOrderRepository.existsById(id)) {
+      // TODO own Exception
+      throw new RuntimeException();
+    }
+
+    PriceForIndividualOrder price = priceForIndividualOrderRepository.getOne(id);
+
+    price.setProductType(request.getProductType());
+    price.setPrice(request.getPrice());
+    price.setDeleted(request.isDeleted());
+    price.setChanged(new Timestamp(System.currentTimeMillis()));
+
+    return priceForIndividualOrderRepository.save(price);
+  }
 }
