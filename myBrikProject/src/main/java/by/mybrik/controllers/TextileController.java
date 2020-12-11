085ee0ea5528eb0872e15de654be6f5e922f55ca
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-11 21:59:08 +0300

    Replaced implementation from Hibernate to Spring Data: UsersController implemented methods: addNewTextile(), updateTextile()

--- myBrikProject/src/main/java/by/mybrik/controllers/TextileController.java
+++ myBrikProject/src/main/java/by/mybrik/controllers/TextileController.java
@@ -1,5 +1,7 @@
 package by.mybrik.controllers;
 
+import by.mybrik.controllers.requests.textileRequests.TextileCreate;
+import by.mybrik.controllers.requests.textileRequests.TextileUpdate;
 import by.mybrik.domain.Textile;
 import by.mybrik.repository.impl.TextileRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,10 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
@@ -48,61 +54,62 @@ public class TextileController {
     return textileRepository.findAll();
   }
 
-  //  /*
-  //  http://localhost:8080/new/rest/textile/
-  //  {
-  //      "code": "someCode89",
-  //      "name": "someName69",
-  //      "color": "someColor",
-  //      "description": "someDescription",
-  //      "photo": "somePhoto",
-  //      "deleted": false
-  //  }
-  //   */
-  //  @PostMapping
-  //  @ResponseStatus(HttpStatus.CREATED)
-  //  public Textile addNewTextile(@RequestBody TextileCreate request) {
-  //
-  //    Textile textile = new Textile();
-  //
-  //    textile.setCode(request.getCode());
-  //    textile.setName(request.getName());
-  //    textile.setColor(request.getColor());
-  //    textile.setDescription(request.getDescription());
-  //    textile.setPhoto(request.getPhoto());
-  //    textile.setDeleted(request.isDeleted());
-  //
-  //    return textileService.save(textile);
-  //  }
-  //
-  //  /*
-  //  http://localhost:8080/new/rest/textile/6
-  //   {
-  //      "code": "someCodeUpdate",
-  //      "name": "someName6",
-  //      "color": "someColor",
-  //      "description": "someDescription",
-  //      "photo": "somePhoto",
-  //      "deleted": false
-  //  }
-  //   */
-  //  @PutMapping("/{id}")
-  //  @ResponseStatus(HttpStatus.OK)
-  //  public Textile updateTextile(@PathVariable("id") Long id, @RequestBody TextileUpdate request)
-  // {
-  //
-  //    Textile updatedTextile = textileService.findById(id);
-  //
-  //    updatedTextile.setCode(request.getCode());
-  //    updatedTextile.setName(request.getName());
-  //    updatedTextile.setColor(request.getColor());
-  //    updatedTextile.setDescription(request.getDescription());
-  //    updatedTextile.setPhoto(request.getPhoto());
-  //    updatedTextile.setDeleted(request.isDeleted());
-  //    updatedTextile.setChanged(new Timestamp(System.currentTimeMillis()));
-  //
-  //    return textileService.update(updatedTextile);
-  //  }
-  //
+  /*
+  http://localhost:8080/new/rest/textile/
+  {
+      "code": "someCode89",
+      "name": "someName69",
+      "color": "someColor",
+      "description": "someDescription",
+      "photo": "somePhoto",
+      "deleted": false
+  }
+   */
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Textile addNewTextile(@RequestBody TextileCreate request) {
 
+    Textile textile = new Textile();
+
+    textile.setCode(request.getCode());
+    textile.setName(request.getName());
+    textile.setColor(request.getColor());
+    textile.setDescription(request.getDescription());
+    textile.setPhoto(request.getPhoto());
+    textile.setDeleted(request.isDeleted());
+
+    return textileRepository.save(textile);
+  }
+
+  /*
+  http://localhost:8080/new/rest/textile/6
+   {
+      "code": "someCodeUpdate",
+      "name": "someName6",
+      "color": "someColor",
+      "description": "someDescription",
+      "photo": "somePhoto",
+      "deleted": false
+  }
+   */
+  @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public Textile updateTextile(@PathVariable("id") Long id, @RequestBody TextileUpdate request) {
+    if (!textileRepository.existsById(id)) {
+      // TODO own Exception
+      throw new RuntimeException();
+    }
+
+    Textile updatedTextile = textileRepository.getOne(id);
+
+    updatedTextile.setCode(request.getCode());
+    updatedTextile.setName(request.getName());
+    updatedTextile.setColor(request.getColor());
+    updatedTextile.setDescription(request.getDescription());
+    updatedTextile.setPhoto(request.getPhoto());
+    updatedTextile.setDeleted(request.isDeleted());
+    updatedTextile.setChanged(new Timestamp(System.currentTimeMillis()));
+
+    return textileRepository.save(updatedTextile);
+  }
 }
