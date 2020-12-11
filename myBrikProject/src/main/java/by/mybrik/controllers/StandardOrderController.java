80844b9030435d53f0817830d63f96fa4c557e8d
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-11 22:43:39 +0300

    Replaced implementation from Hibernate to Spring Data: StandardOrderController implemented methods: createStandardOrder(), updateStandardOrder(),

--- myBrikProject/src/main/java/by/mybrik/controllers/StandardOrderController.java
+++ myBrikProject/src/main/java/by/mybrik/controllers/StandardOrderController.java
@@ -1,5 +1,6 @@
 package by.mybrik.controllers;
 
+import by.mybrik.controllers.requests.standardOrderRequests.StandardOrderCreate;
 import by.mybrik.domain.StandardOrder;
 import by.mybrik.repository.impl.StandardOrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,10 +8,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
@@ -47,55 +52,59 @@ public class StandardOrderController {
     return standardOrderRepository.findAll();
   }
 
-  //  /*
-  //  http://localhost:8080/new/rest/standardorder
-  //  {
-  //      "goodId": 6,
-  //      "userId": 10,
-  //      "quantity": 12,
-  //      "totalPrice": 12.0,
-  //      "orderStatus": "in progress",
-  //  }
-  //   */
-  //  @PostMapping
-  //  @ResponseStatus(HttpStatus.CREATED)
-  //  public StandardOrder createStandardOrder(@RequestBody StandardOrderCreate request) {
-  //
-  //    StandardOrder order = new StandardOrder();
-  //
-  //    order.setGoodId(request.getGoodId());
-  //    order.setUserId(request.getUserId());
-  //    order.setQuantity(request.getQuantity());
-  //    order.setTotalPrice(request.getTotalPrice());
-  //    order.setOrderStatus(request.getOrderStatus());
-  //
-  //    return orderService.save(order);
-  //  }
-  //
-  //  /*
-  //  http://localhost:8080/new/rest/standardorder/4
-  //  {
-  //          "goodId": 6,
-  //          "userId": 10,
-  //          "quantity": 115,
-  //          "totalPrice": 1200.0,
-  //          "orderStatus": "in progress"
-  //  }
-  //   */
-  //  @PutMapping("/{id}")
-  //  @ResponseStatus(HttpStatus.OK)
-  //  public StandardOrder updateStandardOrder(
-  //      @PathVariable Long id, @RequestBody StandardOrderCreate request) {
-  //
-  //    StandardOrder updateOrder = orderService.findById(id);
-  //
-  //    updateOrder.setGoodId(request.getGoodId());
-  //    updateOrder.setUserId(request.getUserId());
-  //    updateOrder.setQuantity(request.getQuantity());
-  //    updateOrder.setTotalPrice(request.getTotalPrice());
-  //    updateOrder.setOrderStatus(request.getOrderStatus());
-  //    updateOrder.setChanged(new Timestamp(System.currentTimeMillis()));
-  //
-  //    return orderService.update(updateOrder);
-  //  }
+  /*
+  http://localhost:8080/new/rest/standardorder
+  {
+      "goodId": 6,
+      "userId": 10,
+      "quantity": 12,
+      "totalPrice": 12.0,
+      "orderStatus": "in progress"
+  }
+   */
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public StandardOrder createStandardOrder(@RequestBody StandardOrderCreate request) {
+
+    StandardOrder order = new StandardOrder();
+
+    order.setGoodId(request.getGoodId());
+    order.setUserId(request.getUserId());
+    order.setQuantity(request.getQuantity());
+    order.setTotalPrice(request.getTotalPrice());
+    order.setOrderStatus(request.getOrderStatus());
+
+    return standardOrderRepository.save(order);
+  }
+
+  /*
+  http://localhost:8080/new/rest/standardorder/4
+  {
+          "goodId": 6,
+          "userId": 10,
+          "quantity": 115,
+          "totalPrice": 1200.0,
+          "orderStatus": "in progress"
+  }
+   */
+  @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public StandardOrder updateStandardOrder(
+      @PathVariable Long id, @RequestBody StandardOrderCreate request) {
+    if (!standardOrderRepository.existsById(id)) {
+      // TODO own Exception
+      throw new RuntimeException();
+    }
+
+    StandardOrder updateOrder = standardOrderRepository.getOne(id);
+
+    updateOrder.setGoodId(request.getGoodId());
+    updateOrder.setUserId(request.getUserId());
+    updateOrder.setQuantity(request.getQuantity());
+    updateOrder.setTotalPrice(request.getTotalPrice());
+    updateOrder.setOrderStatus(request.getOrderStatus());
+    updateOrder.setChanged(new Timestamp(System.currentTimeMillis()));
+
+    return standardOrderRepository.save(updateOrder);
+  }
 }
