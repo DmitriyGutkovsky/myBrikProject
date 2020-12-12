0df2cc798c2892d768277a277fe84ee62a3b691a
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-12 17:08:11 +0300

    Security: configuration UserServiceProvider (UserDetailsService)

--- myBrikProject/src/main/java/by/mybrik/repository/impl/RoleRepository.java
+++ myBrikProject/src/main/java/by/mybrik/repository/impl/RoleRepository.java
@@ -3,5 +3,8 @@ package by.mybrik.repository.impl;
 import by.mybrik.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    List<Role> findUserRoles(Long userId);
 }
