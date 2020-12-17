83c318ff23b7b096c935fd698f43811836b4754f
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-17 10:59:39 +0300

    Security: JwtTokenFilter cleaning

--- myBrikProject/src/main/java/by/mybrik/security/filters/JwtTokenFilter.java
+++ myBrikProject/src/main/java/by/mybrik/security/filters/JwtTokenFilter.java
@@ -4,10 +4,8 @@ import by.mybrik.domain.Role;
 import by.mybrik.domain.SystemRoles;
 import by.mybrik.repository.impl.UsersRepository;
 import by.mybrik.security.CustomHeaders;
-import by.mybrik.security.service.UserServiceProvider;
 import by.mybrik.security.util.TokenUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -27,53 +25,37 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
 
-    private final TokenUtils tokenUtils;
+  private final TokenUtils tokenUtils;
 
-    private final UsersRepository usersRepository;
+  private final UsersRepository usersRepository;
 
-//    @Autowired
-//    UserServiceProvider  userServiceProvider;
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
 
-//    public JwtTokenFilter(TokenUtils tokenUtils) {
-//        this.tokenUtils = tokenUtils;
-//    }
+    final String jwt = request.getHeader(CustomHeaders.AUTH_HEADER);
 
+    String login = null;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
-
-//        final String authorizationHeader  = request.getHeader(CustomHeaders.AUTH_HEADER);
-        final String jwt  = request.getHeader(CustomHeaders.AUTH_HEADER);
-
-
-        String login = null;
-//        String jwt = null;
-
-        if (jwt != null){
-            login = tokenUtils.getUsernameFromToken(jwt);
-        }
-
-        if (login != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//            String commaSeparatedListOfAuthorities = tokenUtils.extractAuthorities(jwt);
-
-
-            List<GrantedAuthority> grantedAuthorities = AuthorityUtils.commaSeparatedStringToAuthorityList(usersRepository.findByLogin(login).get().getRoles().stream()
-                    .map(Role::getRoleName).map(SystemRoles::name)
-                    .collect(Collectors.joining(",")));
-
-
-//            List<GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList(commaSeparatedListOfAuthorities);
-            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                    new UsernamePasswordAuthenticationToken(
-                            login, null,
-//                            authorities
-                            grantedAuthorities);
-
-            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-        }
-        filterChain.doFilter(request, response);
-
+    if (jwt != null) {
+      login = tokenUtils.getUsernameFromToken(jwt);
     }
+
+    if (login != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+      List<GrantedAuthority> grantedAuthorities =
+          AuthorityUtils.commaSeparatedStringToAuthorityList(
+              usersRepository.findByLogin(login).get().getRoles().stream()
+                  .map(Role::getRoleName)
+                  .map(SystemRoles::name)
+                  .collect(Collectors.joining(",")));
+
+      UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+          new UsernamePasswordAuthenticationToken(login, null, grantedAuthorities);
+
+      SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+    }
+    filterChain.doFilter(request, response);
+  }
 }
