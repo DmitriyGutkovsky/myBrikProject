434b70976fe1847027bbfed986036ee8ece09aae
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-28 16:32:25 +0300

    Created class for handling own exceptions - EntityNotFoundException

---
+++ myBrikProject/src/main/java/by/mybrik/exceptions/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package by.mybrik.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+  public EntityNotFoundException(String message) {
+    super(message);
+  }
+
+  public EntityNotFoundException() {
+    super();
+  }
+}
